c77229f7a87d09efd21e521cc41854f96a1224e8
what <diana109ri@gmail.com>
2026-03-30 07:27:11 +0000

    ioy

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const db = new Database("./database.db");
 
 // Включаем поддержку внешних ключей
-db.pragma('foreign_keys = ON');
+db.pragma('foreign_keys = ON'); 
 
 
 // Мой замечательный супер пупер код
@@ -40,70 +40,4 @@ db.exec(`
         FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
     );
 `);
-
-function seedDatabase() {
-
-    // Проверяем, есть ли уже Земфирка (пользователь)
-    const userCount = db.prepare("SELECT COUNT(*) as count FROM users").get().count;
-    if (userCount > 0) {
-        console.log("Ура, Земфира на сцене!");
-        return;
-    }
-
-    console.log("Земфира готовиться...");
-
-    // Хэшируем пароли как попросил начальника
-
-    const adminPass = bcrypt.hashSync("qwerty123", 10);
-    const userPass = bcrypt.hashSync("qwerty123", 10);
-
-    // Добавляем Земфирку и Админа
-    const insertUser = db.prepare("INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)");
-    insertUser.run("admin", "Makulatura@gmail.com", adminPass, "admin");
-    insertUser.run("user", "Zemfira@gmail.com", userPass, "user");
-
-    // Краду апельсины у Земфиры
-    const admin = db.prepare("SELECT id FROM users WHERE username = ?").get("admin");
-    const user = db.prepare("SELECT id FROM users WHERE username = ?").get("user");
-
-    const insertBook = db.prepare(`
-        INSERT INTO books (title, author, year, genre, description, createdBy)
-        VALUES (?, ?, ?, ?, ?, ?)
-    `);
-    const books = [
-        ["Нейт диаз", "Макулатура",  2016, "хип-хоп", "рэп", admin.id],
-        ["Вечеринка у Децла", "Detsl", 1999 , "хип-хоп", "рэп", admin.id],
-        ["ПЛМ", "Мелатонин", 2024, "альтернативный рок", "амбасадор", admin.id],
-        ["Ice Baby", "Гуф", 2009, "хип-хоп", "рэп", admin.id],
-        ["Heavenly", " Cigarettes After Sex", 2019, "рок", "поп", user.id],
-    ];
-    // Это все правда книги так женя сказала
-
-
-    const bookIds = [];
-    for (const book of books) {
-        const info = insertBook.run(...book);
-        bookIds.push(info.lastInsertRowid);
-    }
-
-    // Делаем отзывы 
-    const insertReview = db.prepare(`
-        INSERT INTO reviews (bookId, userId, rating, comment) VALUES (?, ?, ?, ?)
-    `);
-    const reviews = [
-        [bookIds[0], user.id, 5, "Словил тильт, мне понравиллось"],
-        [bookIds[1], user.id, 4, "Лучше чем ничего"],
-        [bookIds[2], user.id, 5, "Эээ...ну да вроде норм"],
-        [bookIds[3], admin.id, 5, "Лучше чем балерина капучино"],
-        [bookIds[4], admin.id, 4, "ОГО ЗДЕСЬ НЕТ ЦЕНЗУРЫ"],
-    ];
-    for (const review of reviews) {
-        insertReview.run(...review);
-    }
-
-    console.log("Да поменял я все.");
-}
-
-
-seedDatabase();
 module.exports = db;
